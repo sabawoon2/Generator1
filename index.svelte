40319cd5404c0fe761740cd29d61f560b3138f70
sabawoon2 <58486387+sabawoon2@users.noreply.github.com>
2021-06-28 22:26:40 +0200

    Add files via upload

---
+++ index.svelte
@@ -0,0 +1,299 @@
+<script>
+	let RandomNumber;
+	let RandomText;
+		
+	
+	
+	function random() {
+		RandomNumber= Math.floor(Math.random() * 75); 
+		text();
+	
+	}
+	
+	function text() {
+		if (RandomNumber==1) {
+			RandomText="Du er pen";
+			
+		}
+		else if (RandomNumber==2) {
+			RandomText="Du er søt";
+			
+		}
+		else if (RandomNumber==3) {
+			RandomText="Du er verdt 100000*900^20 kameler";
+			
+		}else if (RandomNumber==4) {
+			RandomText="Du lukter fnis";
+			
+		}else if (RandomNumber==5) {
+			RandomText="Mogonlia er bæsjebæsj";
+			
+		}else if (RandomNumber==6) {
+			RandomText="Pakistan er prompepromp";
+			
+		}else if (RandomNumber==7) {
+			RandomText="Du gjør meg glad";
+			
+		}else if (RandomNumber==8) {
+			RandomText="Drikk vann fss";
+			
+		}else if (RandomNumber==9) {
+			RandomText="Hunden din er den søteste chihuahuaen noensinne ";
+			
+		}else if (RandomNumber==10) {
+			RandomText="Smilet ditt gjør meg glag";
+			
+		}else if (RandomNumber==11) {
+			RandomText="Leppene dine er så jøvlig fine bruh";
+			
+		}else if (RandomNumber==12) {
+			RandomText="Du er søt";
+			
+		}else if (RandomNumber==13) {
+			RandomText="Pannen din får meg til å cumme i buksa";
+			
+		}else if (RandomNumber==14) {
+			RandomText="Du er så flink! I fakkings alt! Stopp!";
+			
+		}else if (RandomNumber==15) {
+			RandomText="Kroppen din er hawt!";
+			
+		}else if (RandomNumber==16) {
+			RandomText="Nesen din er så jævlig søt";
+			
+		}else if (RandomNumber==17) {
+			RandomText="Tiden går altfor fort når jeg er med deg";
+			
+		}else if (RandomNumber==18) {
+			RandomText="Du er en sussy baka";
+			
+		}else if (RandomNumber==19) {
+			RandomText="Du er søt";
+			
+		}else if (RandomNumber==20) {
+			RandomText="Jeg får selvmordstanker hver gang du nevner Aziz";
+			
+		}else if (RandomNumber==21) {
+			RandomText="Du blæster meg i 8pool... stopp :(";
+			
+		}else if (RandomNumber==22) {
+			RandomText="Dine skills på piano er bare noe helt annet ";
+			
+		}else if (RandomNumber==23) {
+			RandomText="Måten du går på er så jævlig smexy, stopp!";
+			
+		}else if (RandomNumber==24) {
+			RandomText="Du er som den siste nuggeten i en 12 pakker";
+			
+		}else if (RandomNumber==25) {
+			RandomText="Du er så morsom";
+			
+		}else if (RandomNumber==26) {
+			RandomText="Du er så energisk, jeg elsker det";
+			
+		}else if (RandomNumber==27) {
+			RandomText="Karoline og Henrik får meg til å hate meg selv";
+			
+		}else if (RandomNumber==28) {
+			RandomText="Du er mæd wifey";
+			
+		}else if (RandomNumber==29) {
+			RandomText="Jeg kan være meg selv rundt deg";
+			
+		}else if (RandomNumber==30) {
+			RandomText="Kyllingdansen din er imponerende bruh";
+			
+		}else if (RandomNumber==31) {
+			RandomText="Du får meg til å simpe";
+			
+		}else if (RandomNumber==32) {
+			RandomText="Det er mæd beroligende når du synger";
+			
+		}else if (RandomNumber==33) {
+			RandomText="Du er typen som brenner egg når du steker det";
+			
+		}else if (RandomNumber==34) {
+			RandomText="Når du forkalrer vitser får jeg lyst til å dø";
+			
+		}else if (RandomNumber==35) {
+			RandomText="Vafflene dine får meg til å cumme";
+			
+		}else if (RandomNumber==36) {
+			RandomText="Du suger i å overleve";
+			
+		}else if (RandomNumber==37) {
+			RandomText="Bæsje bæsj";
+			
+		}else if (RandomNumber==38) {
+			RandomText="Æsj, bæsj, promp og fis";
+			
+		}else if (RandomNumber==39) {
+			RandomText="Du er så sterk";
+			
+		}else if (RandomNumber==40) {
+			RandomText="Du er så modig";
+			
+		}else if (RandomNumber==41) {
+			RandomText="Du er så ambisiøs";
+			
+		}else if (RandomNumber==42) {
+			RandomText="Jeg digger deg altfor mye";
+			
+		}else if (RandomNumber==43) {
+			RandomText="Man finner ikke mange som deg";
+			
+		}else if (RandomNumber==44) {
+			RandomText="Fakk snapp, få mailen din";
+			
+		}else if (RandomNumber==45) {
+			RandomText="Tenk at du ikke kan svømme, lol";
+			
+		}else if (RandomNumber==46) {
+			RandomText="Oog Boog!";
+			
+		}else if (RandomNumber==47) {
+			RandomText="Jeg elsker deg (bare lowkey da)";
+			
+		}else if (RandomNumber==48) {
+			RandomText="Du har så mye empati ";
+			
+		}else if (RandomNumber==49) {
+			RandomText="Håret ditt er så fint";
+			
+		}else if (RandomNumber==50) {
+			RandomText="Når jeg har hatt en shitty dag, tenker jeg på deg for å gjøre dagen min bedre";
+			
+		}else if (RandomNumber==51) {
+			RandomText="Du er beast i geografi";
+			
+		}else if (RandomNumber==52) {
+			RandomText="Jeg elsker dine funfacts";
+			
+		}else if (RandomNumber==53) {
+			RandomText="Du er jenta mi";
+			
+		}else if (RandomNumber==54) {
+			RandomText="du lukter så jævlig digg";
+			
+		}else if (RandomNumber==55) {
+			RandomText="Du er altfor Haaaawt";
+			
+		}else if (RandomNumber==56) {
+			RandomText="Drippen din er mæææd smexy";
+			
+		}else if (RandomNumber==57) {
+			RandomText="Moren din";
+			
+		}else if (RandomNumber==58) {
+			RandomText="Fish, Am I right?";
+			
+		}else if (RandomNumber==59) {
+			RandomText="Jeg pisser på meeeeeeg";
+			
+		}else if (RandomNumber==60) {
+			RandomText="uuuh okey";
+			
+		}else if (RandomNumber==61) {
+			RandomText="Vidar er bæsjebæsj ";
+			
+		}else if (RandomNumber==62) {
+			RandomText="Du er flink til å danse";
+			
+		}else if (RandomNumber==63) {
+			RandomText="Du er så sjuk flink til å tegne";
+			
+		}else if (RandomNumber==64) {
+			RandomText="Hvordan er du perfekt i alle områder?";
+			
+		}else if (RandomNumber==65) {
+			RandomText="Det at du ser på dokumentarer er orgasmisk ";
+			
+		}else if (RandomNumber==66) {
+			RandomText="Digger humoren din";
+			
+		}else if (RandomNumber==67) {
+			RandomText="Navelen din er sjuk fin, wtf";
+			
+		}else if (RandomNumber==68) {
+			RandomText="Marte Boneparte";
+			
+		}else if (RandomNumber==69) {
+			RandomText="Tenk at du egentlig heter Oda";
+			
+		}else if (RandomNumber==70) {
+			RandomText="Jeg gråt i dusjen når du shava barten din :`)";
+			
+		}else if (RandomNumber==71) {
+			RandomText="ew, Asian";
+			
+		}else if (RandomNumber==72) {
+			RandomText="Shawty a badyyyy";
+			
+		}else if (RandomNumber==73) {
+			RandomText="promperumpe!";
+			
+		}else if (RandomNumber==74) {
+			RandomText="hah, simp";
+			
+	
+		
+	  
+	}
+}
+		
+	</script>
+	
+	<body>
+	
+		<div class="App">
+		
+			<h1 id="overskrift" >Marte generator</h1>
+			
+			<button class="knapp" on:click={random} >Trykk!</button>    
+			
+		
+			<p class="text"> {RandomText} </p>
+		 
+			
+			</div>
+			
+		
+	
+	
+	</body>
+	
+	
+	
+	
+	<style>
+		body{
+			background-image: url(https://www.mpg.de/16679552/original-1617795150.webp?t=eyJ3aWR0aCI6MTY5NiwiZmlsZV9leHRlbnNpb24iOiJ3ZWJwIiwib2JqX2lkIjoxNjY3OTU1Mn0%3D--d22ec65216cea068c335c90ff1118de117ae6c8b);
+		}
+		#overskrift{
+			display: flex;
+			justify-content: center;
+			font-size: 50px;
+			color: black;
+	
+		}
+		.knapp{
+			margin-left: 330px;
+			padding: 50px;
+			border-style: solid;
+			background-color:aqua;
+			border-radius: 100px;
+		}
+		.text{
+			display: flex;
+			justify-content: center;
+			
+		}
+		.App{
+			border-style: solid;
+			background-image: url(https://png.pngtree.com/thumb_back/fw800/background/20190223/ourmid/pngtree-pure-color-watercolor-graffiti-gradient-background-board-design-board-design-image_66713.jpg);
+		}
+	  
+	
+	
+	
+	</style>
